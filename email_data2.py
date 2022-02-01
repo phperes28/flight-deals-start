02790ffab15ba7c7d30721121e6a177644750741
phperes28 <84190778+phperes28@users.noreply.github.com>
2022-02-01 17:03:46 -0300

    Delete email_data2.py

--- email_data2.py
+++
@@ -1,7 +0,0 @@
-MY_EMAIL = "pedrodev28@gmail.com"
-SENHA = "Shark289*"
-
-us_number = '+12086001119'
-my_number = '+5561999854775'
-auth_token = '5fad40655124ff083385048fc8d4ae23'
-account_sid = 'WEATHER_KEY'
